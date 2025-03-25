ed3e8ad4e12ec39d1ea5485c908387c7ff87e8e5
Andrew Lamb <andrew@nerdnetworks.org>
2025-03-25 14:06:42 -0400

    Update rand requirement from 0.8 to 0.9 (try 2) (#7126)
    * Reapply "Update rand requirement from 0.8 to 0.9 (#7045)"
    
    This reverts commit 71a47704996668327f69b72bf803b5dd6b244e9e.
    
    * Revert "downgrade API"
    
    This reverts commit 352e24919843f204ce80daf60a2ebdc8bc0629f0.
    
    * Apply suggestions from code review
    
    Co-authored-by: Matthijs Brobbel <m1brobbel@gmail.com>
    
    ---------
    
    Co-authored-by: Matthijs Brobbel <m1brobbel@gmail.com>

--- parquet/src/arrow/async_reader/mod.rs
+++ parquet/src/arrow/async_reader/mod.rs
@@ -1069,7 +1069,7 @@ mod tests {
     };
     use arrow_schema::{DataType, Field, Schema};
     use futures::{StreamExt, TryStreamExt};
-    use rand::{thread_rng, Rng};
+    use rand::{rng, Rng};
     use std::collections::HashMap;
     use std::sync::{Arc, Mutex};
     use tempfile::tempfile;
@@ -1399,7 +1399,7 @@ mod tests {
 
         assert_eq!(metadata.num_row_groups(), 1);
 
-        let mut rand = thread_rng();
+        let mut rand = rng();
 
         for _ in 0..100 {
             let mut expected_rows = 0;
@@ -1408,7 +1408,7 @@ mod tests {
             let mut selectors = vec![];
 
             while total_rows < 7300 {
-                let row_count: usize = rand.gen_range(1..100);
+                let row_count: usize = rand.random_range(1..100);
 
                 let row_count = row_count.min(7300 - total_rows);
 
@@ -1435,7 +1435,7 @@ mod tests {
                 .await
                 .unwrap();
 
-            let col_idx: usize = rand.gen_range(0..13);
+            let col_idx: usize = rand.random_range(0..13);
             let mask = ProjectionMask::leaves(builder.parquet_schema(), vec![col_idx]);
 
             let stream = builder
@@ -1466,7 +1466,7 @@ mod tests {
 
         assert_eq!(metadata.num_row_groups(), 1);
 
-        let mut rand = thread_rng();
+        let mut rand = rng();
 
         let mut expected_rows = 0;
         let mut total_rows = 0;
@@ -1479,7 +1479,7 @@ mod tests {
         });
 
         while total_rows < 7300 {
-            let row_count: usize = rand.gen_range(1..100);
+            let row_count: usize = rand.random_range(1..100);
 
             let row_count = row_count.min(7300 - total_rows);
 
@@ -1506,7 +1506,7 @@ mod tests {
             .await
             .unwrap();
 
-        let col_idx: usize = rand.gen_range(0..13);
+        let col_idx: usize = rand.random_range(0..13);
         let mask = ProjectionMask::leaves(builder.parquet_schema(), vec![col_idx]);
 
         let stream = builder
